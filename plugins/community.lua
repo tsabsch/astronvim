710b034b4555a125a0c9eac855a625842ec9fbfb
Tim Sabsch <tim.sabsch@codecentric.de>
2024-01-10 09:44:04 +0100

    Add config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,12 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.pack.full-dadbod" },
 }
